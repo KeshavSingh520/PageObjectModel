fe75345bd534a6b07c63d9a381ebaa5e947cdae1
unknown <you@example.com>
2018-06-16 16:49:31 +0530

    First Day Commit

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,32 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class HomePage {
+	public static  WebDriver driver;
+	
+	public HomePage(WebDriver  driver)
+	{
+		this.driver=driver;
+		
+	}
+	
+	
+	@Test
+	public void doLogin(String username,String password)
+	{
+		driver.findElement(By.cssSelector("input[type='email']")).sendKeys(username);
+		driver.findElement(By.cssSelector("input[name='pass']")).sendKeys(password);
+	}
+	
+	public void createaccount()
+	{
+		
+			System.out.println("creating account");
+	}
+	
+	
+
+}
