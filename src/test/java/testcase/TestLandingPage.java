a5ef081d34f324c7cf6474a802881cc437df522c
unknown <you@example.com>
2018-07-22 11:57:19 +0530

    ksbv

--- src/test/java/testcase/TestLandingPage.java
+++ src/test/java/testcase/TestLandingPage.java
@@ -10,8 +10,8 @@ public class TestLandingPage {
 	@Test
 	public void Testlogin()
 	{
-		LandingPage LP =new LandingPage();
-		LP.doSearch();
+		LandingPage LP1 =new LandingPage();
+		LP1.doSearch();
 		
 	}
 
